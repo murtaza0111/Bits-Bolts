f982319ad2cabe98df17d9178a0ad574423be9f7
murtaza0111 <69841841+murtaza0111@users.noreply.github.com>
2023-03-08 21:44:53 +0500

    Delete 4- Source Code directory

--- 4- Source Code/src/custom/utils/CheckSession.js
+++
@@ -1,6 +0,0 @@
-import { getLoginCredentialsFromLocalStorage } from "./localStorage/localStorage";
-
-export const checkToken = async (navigate, route) => {
-  const credential = await getLoginCredentialsFromLocalStorage();
-  return credential.token === "" ? navigate(route, { replace: true }) : true;
-};
