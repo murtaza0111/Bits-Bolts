3fb53df35e3ed4637dcbafdaaf9b199ed1758c48
murtaza0111 <69841841+murtaza0111@users.noreply.github.com>
2023-03-09 15:45:18 +0500

    Delete src directory

--- src/custom/utils/message.js
+++
@@ -1,4 +0,0 @@
-export function messageDialogue(element, messageType, messageColor, msg) {
-  document.querySelector(`${element}`).innerHTML = `
-    <span style="display:${messageType};color:${messageColor};font-size:1.2rem;">${msg}</span>`;
-}
